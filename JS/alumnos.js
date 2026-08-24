033954efcda52d136fee954bd5dc6eb2292d2ccd
AxelOchoa <ochoa.axeldiego@gmail.com>
2026-08-24 15:51:36 -0300

    clase 11 - Cambio visual y comienzo refactorizacion

---
+++ JS/alumnos.js
@@ -0,0 +1,194 @@
+// const alumnos = [
+//     {
+//         id: 1,
+//         nombre: "Ana"
+//     },
+//     {
+//         id: 2,
+//         nombre: "José"
+//     }
+// ];
+// function obtenerAlumnos(){
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve(alumnos)
+//         }, 2000);
+//     })
+// }
+
+// crear obtenerMaterias()
+// crear obtenerDocentes()
+// mostrar los datos a través de async/await
+
+// async function obtenerAlumnos() {
+//     const respuesta = await fetch("https://jsonplaceholder.typicode.com/users")
+//     const alumnos = await respuesta.json()
+//     return alumnos
+// }
+
+// function mostrarAlumnos(alumnos){
+// //    console.table(alumnos)
+// console.log(typeof alumnos)
+// localStorage.setItem("alumnos", JSON.stringify(alumnos))
+// const datos = localStorage.getItem("alumnos")
+// console.log(typeof datos)
+// console.log(datos)
+// const alumnosRecuperados = JSON.parse(datos)
+// console.log(typeof alumnosRecuperados)
+// console.table(alumnosRecuperados)
+
+// //    console.log(alumnos[5])
+// // for (const alumno of alumnos){
+// //     console.log(alumno.id, alumno.name, alumno.email)
+// // }
+// }
+
+// async function inciar(){
+//     const alumnos = await obtenerAlumnos()  
+//     mostrarAlumnos(alumnos)
+// }
+
+// inciar()
+
+const formulario = document.querySelector("#formAlumno")
+const mensaje = document.querySelector("#mensaje")
+const listaAlumnos = document.querySelector("#listaAlumnos")
+let alumnoEditandoId = null
+
+formulario.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const nombre = document.querySelector("#nombre").value.trim()
+    const carrera = document.querySelector("#carrera").value.trim()
+    const correo = document.querySelector("#correo").value.trim()
+
+    if (nombre === "" || carrera === "" || correo === "") {
+        mostrarMensaje("Todos los campos son obligatorios", "mje-error")
+        return
+    }
+
+    if (!correo.includes("@")) {
+        mostrarMensaje("Ingrese un correo electrónico válido", "mje-error")
+        return
+    }
+
+    if (nombre.length < 3) {
+        mostrarMensaje("El nombre debe tener al menos 3 caracteres", "mje-error")
+        return
+    }
+
+    const alumnos = obtenerAlumnos()
+
+    if (alumnoEditandoId === null) {
+        const alumno = {
+            id: Date.now(),
+            nombre: nombre,
+            carrera: carrera,
+            correo: correo
+        }
+        alumnos.push(alumno)
+        mostrarMensaje("Alumno guardado correctamente", "mje-exito")
+    } else {
+        const alumno = alumnos.find(alumno => alumno.id === alumnoEditandoId)
+        alumno.nombre = nombre
+        alumno.carrera = carrera
+        alumno.correo = correo
+        alumnoEditandoId = null
+        formulario.querySelector("button").textContent = "Guardar Alumno"
+
+        mostrarMensaje("Alumno actualizado correctamente", "mje-exito")
+    }
+    localStorage.setItem("alumnos", JSON.stringify(alumnos))
+    mostraAlumnos(alumnos)
+    formulario.reset()
+});
+
+
+function obtenerAlumnos() {
+    const datos = localStorage.getItem("alumnos")
+    if (datos) {
+        return JSON.parse(datos)
+    }
+    return []
+}
+
+function mostrarMensaje(texto, clase) {
+    mensaje.textContent = texto;
+    mensaje.className = `mensaje $(clase)`
+    mensaje.style.display = "block"
+        setTimeout(() => {
+        mensaje.style.display = "none"
+    }, 3000);
+}
+
+function mostraAlumnos(alumnos) {
+    listaAlumnos.innerHTML = ""
+    for (const alumno of alumnos) {
+        listaAlumnos.innerHTML += `
+        <tr>
+            <td>${alumno.id}</td>
+            <td>${alumno.nombre}</td>
+            <td>${alumno.carrera}</td>
+            <td>${alumno.correo}</td>
+            <td>
+                <button 
+                class="btn-editar" 
+                data-id="${alumno.id}"
+                title="Editar alumno">
+                <i class="fa-solid fa-pen"></i>
+                </button>
+                <button 
+                class="btn-eliminar" 
+                data-id="${alumno.id}"
+                title="Eliminar alumno">
+                <i class="fa-solid fa-trash"></i>
+                </button>
+            </td>
+        </tr>
+        `;
+    }
+}
+function eliminarAlumno(id) {
+    const alumnos = obtenerAlumnos()
+    const alumnosActualizados = alumnos.filter(
+        alumno => alumno.id !== id
+    );
+    localStorage.setItem("alumnos", JSON.stringify(alumnosActualizados))
+    mostraAlumnos(alumnosActualizados)
+    if (alumnoEditandoId === id){
+        formulario.reset()
+        alumnoEditandoId = null
+        formulario.querySelector("button").textContent = "Guardar alumno"
+    }
+    mostrarMensaje("Alumno eliminado correctamente", "mje-exito")
+}
+
+listaAlumnos.addEventListener("click", (e) => {
+    const boton_el = e.target.closest(".btn-eliminar")
+    if (boton_el) {
+        const id = Number(boton_el.dataset.id)
+        const confirmar = confirm("¿Está seguro de eliminar este alumno?")
+        if (confirmar) {
+        eliminarAlumno(id)
+        }
+    }
+    const boton_ed = e.target.closest(".btn-editar")
+    if (boton_ed) {
+        const id = Number(boton_ed.dataset.id)
+        editarAlumno(id)
+    }
+})
+
+function editarAlumno(id) {
+    const alumnos = obtenerAlumnos()
+    const alumno = alumnos.find(alumno => alumno.id === id)
+    document.querySelector("#nombre").value = alumno.nombre;
+    document.querySelector("#carrera").value = alumno.carrera;
+    document.querySelector("#correo").value = alumno.correo;
+    alumnoEditandoId = id;
+    formulario.querySelector("button").textContent = "Actualizar Alumno"
+    document.querySelector("#nombre").focus()
+}
+
+const alumnos = obtenerAlumnos()
+mostraAlumnos(alumnos)  
---
+++ README.md
@@ -0,0 +1,64 @@
+# Sistema de Gestión Académica (SGA)
+
+Proyecto desarrollado durante la materia Programación IV.
+
+## Descripción
+
+El Sistema de Gestión Académica (SGA) es una aplicación web que permitirá administrar alumnos, docentes, cursos y materias.
+
+Durante el desarrollo del proyecto se incorporarán progresivamente nuevas tecnologías y funcionalidades.
+
+
+## Objetivos
+
+- Gestionar alumnos.
+- Gestionar docentes.
+- Gestionar cursos.
+- Gestionar materias.
+- Implementar autenticación de usuarios.
+- Consumir una API REST.
+- Persistir la información en MongoDB.
+
+
+## Tecnologías
+
+Actualmente:
+
+- HTML5
+- JavaScript
+- CSS
+
+
+Próximamente:
+- React
+- Node.js
+- Express
+- MongoDB
+
+## Estado del proyecto
+
+- Versión: 
+Clase 10 - Estructura actual
+SGA/
+├── index.html
+├── alumnos.html
+├── docentes.html
+│
+├── css/
+│   └── estilos.css
+│
+└── js/
+    ├── alumnos.js
+    └── docentes.js
+
+## Almacenamiento
+
+- localStorage
+- JSON.stringify()
+- JSON.parse()
+
+## Autor
+
+Irina Agretti
+
+Programación IV
